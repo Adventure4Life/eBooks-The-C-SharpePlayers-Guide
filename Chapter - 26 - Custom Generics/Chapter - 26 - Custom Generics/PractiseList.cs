3d0951ec49e234475948c63a9d049626994bfd16
A4L <45801468+Adventure4Life@users.noreply.github.com>
2018-12-17 16:38:16 +1100

    Generics
    Last Chapter .... I think now is to try my own practical project before moving into the advanced section of the book.

---
+++ Chapter - 26 - Custom Generics/Chapter - 26 - Custom Generics/PractiseList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_26_Custom_Generics
+{
+    class PractiseList<T>
+    {
+        private T[] items = new T[0];
+
+        // return item @ index
+        public T GetItems(int index) { return items[index]; }
+
+        //Add an items to an existing array
+        public void Add(T newItem)
+        {
+            T[] newItemList = new T[items.Length + 1];
+            for (int i=0; i < items.Length; i++)
+            {
+                newItemList[i] = items[i];
+            }
+            newItemList[newItemList.Length - 1] = newItem;
+            items = newItemList;
+        }
+    }
+}
